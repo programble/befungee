547c24a97922c2f727d51ca27fc830aaba136a91
Curtis (Programble) <programble@gmail.com>
2010-01-15 17:31:24 -0500

    Added version info

--- src/befungee.py
+++ src/befungee.py
@@ -20,8 +20,33 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 
+__version__ = "0.2.0a"
+
+from optparse import OptionParser
+import sys
+
+def version_info():
+    # GNU Coding guidelines suggests:
+    # GNU hello 2.3
+    # Copyright (C) 2007 Free Software Foundation, Inc.
+    # License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+    # This is free software: you are free to change and redistribute it.
+    # There is NO WARRANTY, to the extent permitted by law.
+    print "Befungee", __version__
+    print "Copyright (C) 2010 Curtis (Programble) <programble@gmail.com>"
+    print "License GPLv3: GNU GPL version 3 <http://gnu.org/licenses/gpl.html>"
+    print "This is free software: you are free to change and redistribute it."
+    print "There is NO WARRANTY, to the extent permitted by law."
+
 def main():
-	
+    # Parse command line options
+    parser = OptionParser(usage="%prog [options] [file]")
+    parser.add_option("--version", dest="version", action="store_true", default=False, help="Print version info and exit")
+    (options, args) = parser.parse_args()
+    
+    if options.version:
+        version_info()
+        sys.exit()
 
 if __name__ == '__main__':
 	main()
